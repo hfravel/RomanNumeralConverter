6400a3c3ab5aae4548f4faf5ef7f1d1e1be25f4e
Hayden Fravel <fravelhm@appstate.edu>
2021-06-21 16:55:19 -0400

    Works for all valid cases 1-3999

--- RomanConvert.py
+++ RomanConvert.py
@@ -8,43 +8,77 @@ romanNums = {
     "M" : 1000
 }
 
-def romanConvert(num):
+romans = ( "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I")
+integs = (1000,  900, 500,  400, 100,   90,  50,   40,  10,    9,   5,    4,   1)
+
+def romanToInt(num):
     total = 0
-    prev = 0
+    prev = 9999
     curr = 0
     repeat = 0
-    
+    # Loops through each character of the string num
     for x in num:
-       
+        # Saves the curr value of the current letter
         curr = romanNums[x]
         
+        # Checks whether the curr is equal, less, or greater than last one.
         if prev == curr:
             repeat += 1
+            # If it is repeated more than 3 times this is an invalid number.
             if repeat > 3:
                 total = 0
                 break
+            #If a V, L, or D is placed next to itself then it is invalid.
             elif (curr == 5 or curr == 50 or curr == 500) and repeat > 2:
                 total = 0
                 break
+            # If neither of the previous are true then it is valid
             else:
                 total += curr
-        elif curr > prev:
+        elif curr < prev:
+            # This is simplest senario. ex. VI
             total += curr
             repeat = 1            
         else:
-            if total > curr:
+            # Repeat cannot be more than 1 because IX exists but IIX doesn't. 
+            if repeat > 1:
                 total = 0
                 break
-            elif repeat > 1:
-                total = 0
-                break
+            # if the previous is false then the previous number must be cancelled out and subtracted.
             else:
-                total -= 2 * curr
+                total -= 2 * prev
+                total += curr
                 repeat = 1
 
         prev = curr
     
-    print(total)
+    return total
 
-number = raw_input("Give a Roman Numeral: ")
-romanConvert(number)
+def intToRoman(num):
+    romanlen = len(romans)
+    romanNum = ""
+    
+    #loops until the number is 0
+    while num > 0:
+        #Loops though the possible number combinations until one is equal to or less than
+        for x in range(romanlen):
+            if num >= integs[x]:
+                #if it is divisible, to save time why not add it that many times.
+                amt = num / integs[x]
+                romanNum += romans[x] * amt
+                num -= integs[x] * amt
+                break
+    
+    #prints the roman numeral
+    #print(romanNum)
+    return romanNum
+
+tot = True
+for i in range(1, 4000):
+    r = intToRoman(i)
+    n = romanToInt(r)
+    if n!= i:
+        print("{}: {}: {}.".format(i, r, n))
+
+#number = raw_input("Give a Roman Numeral: ")
+#romanToInt(number)
