e15325cf761ae9e1325151a5e9867ea43aefe57f
Hayden Fravel <fravelhm@appstate.edu>
2021-07-11 21:35:07 -0400

    PERFECT

--- RomanConvert.py
+++ RomanConvert.py
@@ -12,6 +12,8 @@ romans = ( "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"
 romanInts = (1000,  900, 500,  400, 100,   90,  50,   40,  10,    9,   5,    4,   1)
 romanChars = ["I", "V", "X", "L", "C", "D", "M"]
 
+# This function takes as input a string of roman numerals and turns
+# it into an integer.
 def romanToInt2(num):
     # total is for the final value, repeat is how many times a letter has 
     # been repeated curr is the current letter value, prev is the previous 
@@ -28,9 +30,48 @@ def romanToInt2(num):
     if numLen > 15:
         return 0
     
+    # If the roman numeral is (I, V, X, L, C, D, M)
+    if numLen == 1:
+        if num[0] in romanChars: return romanNums[num[0]]
+        else: return 0
+
+    # Deals with the base case of 0 and 1 which do not have both a prev and a pprev.
+    # Yes this is duplicated code but I believe it saves on efficiency because 
+    # there is not need for extra checking in the loop whether it is at 0 or 1.
+    if num[0] in romanChars and num[1] in romanChars:
+        pprev = romanNums[num[0]]
+        prev = romanNums[num[1]]
+        
+        # Cases: (VI, XV, XI, LX, LV ... MX, MV, MI)
+        # Further explained in the loop.
+        if pprev > prev:
+            repeat = 1
+            total = prev + pprev
+            maxx = pprev
+        # Cases: (IV, IX, XL, XC, CD, CM)
+        # Further explained in the loop.
+        elif pprev < prev:
+            if pprev in (1, 10, 100) and (prev / pprev in (5, 10)):
+                repeat = 1
+                total = prev - pprev
+                maxx = prev
+            else:
+                return 0
+        # Cases: (II, XX, CC, MM)
+        # Further explained in the loop.
+        else:
+            if prev in (1, 10, 100, 1000):
+                repeat = 2
+                total = 2 * prev
+                maxx = prev
+            else:
+                return 0
+    else:
+        return 0
+
     # Loops through all letters of num, and returns 0 for an invalid
     # roman numeral.
-    for i in range(0, numLen):
+    for i in range(2, numLen):
         # Invalid character detection.
         if num[i] not in romanChars:
             return 0
@@ -38,10 +79,11 @@ def romanToInt2(num):
         # Sets curr to the current value of the current letter.
         curr = romanNums[num[i]]
         
+
         # If the previous set of letter looks like this: (IV, IX, XL,
         # XC, CD, CM) then the curr letter must be less then the
         # previous letter.
-        if i > 1 and pprev < prev and repeat < 2:
+        if pprev < prev:    
             if pprev > curr:
                 repeat = 1
                 total += curr
@@ -50,13 +92,10 @@ def romanToInt2(num):
         # The curr letter can only be greater than the prev one if it is
         # of the form (IV, IX, XL, XC, CD, CM).        
         elif prev < curr:
-            if i == 0:
-                repeat = 1
-                total += curr
-            elif repeat > 1:
+            if repeat > 1:
                 return 0
             elif prev in (1, 10 ,100) and curr / prev in (5, 10):
-                if curr > maxx or (pprev == curr and curr in (5,50,500)) or (curr == maxx and curr in (5, 50, 500)):
+                if curr > maxx or (curr == maxx and curr in (5, 50, 500)):
                     return 0
                 else:
                     repeat = 1
@@ -94,86 +133,13 @@ def romanToInt2(num):
     # and there were no errors.
     return total
 
-"""
-def romanToInt(num):
-    total = 0
-    pprev = 9999
-    prev = 9999
-    curr = 0
-    repeat = 0
-    currlow = 9999
-    prevlow = 9999
-    
-    # Loops through each character of the string num
-    for x in num:
-        # If there is a character that is not I, V, X, L, C, D, or M then it is not valid.
-        if x not in romanChars:
-            total = 0
-            break
-        
-        # Saves the curr value of the current letter
-        curr = romanNums[x]
-        
-        if curr > prev:
-            # Repeat cannot be more than 1 because IX exists but IIX doesn't. 
-            if repeat > 1:
-                total = 0
-                break
-            # Because of hwo the patterns and math works out, the previous letter must be 1/5 or 1/10 or the current.
-            elif (curr / prev == 5 or curr / prev == 10) and prev not in [romanNums["V"], romanNums["L"], romanNums["D"]]:    
-                # Cannot have DCD because this is the same as CM. So a 5 cannot come before a 4.
-                if prevlow == curr and curr in [romanNums["V"], romanNums["L"], romanNums["D"]]:
-                    total = 0
-                    break
-                # If the previous is false then we continue with normal operations.
-                total -= 2 * prev
-                total += curr
-                repeat = 1
-                
-                prevlow = currlow
-                currlow = curr - prev
-            # If neither of the above two are true then this roman numeral is a fake one.
-            else:
-                total = 0
-                break
-        # Roman Numerals are supposed to be organized from greatest on left to least on right.
-        # Ex. MMCM is alright because this is 1000, 1000, 900.  but MMCMM is not because that is 1000, 1000, 900, 1000
-        elif curr > currlow:
-            total = 0
-            break
-        # Checks whether the curr is equal, less, or greater than last one.
-        elif prev == curr:
-            repeat += 1
-            # If it is repeated more than 3 times this is an invalid number.
-            if repeat > 3:
-                total = 0
-                break
-            #If a V, L, or D is placed next to itself then it is invalid.
-            elif curr in [romanNums["V"], romanNums["L"], romanNums["D"]] and repeat > 1:
-                total = 0
-                break
-            # If neither of the previous are true then it is valid
-            else:
-                total += curr
-        else:
-            # This is simplest senario. ex. VI
-            total += curr
-            repeat = 1
 
-            prevlow = currlow
-            currlow = curr
-
-        # End of the loop where we assign previous to current
-        prev = curr
-    
-    return total
-"""
 
 # Takes as input a integer and returns a string representing
 # the roman numeral.
 def intToRoman(num):
     romanlen = len(romans)
-    romanNum = ""
+    result = ""
     
     #loops until the number is 0
     while num > 0:
@@ -182,22 +148,27 @@ def intToRoman(num):
             if num >= romanInts[x]:
                 #if it is divisible, to save time why not add it that many times.
                 amt = num / romanInts[x]
-                romanNum += romans[x] * amt
+                result += romans[x] * amt
                 num -= romanInts[x] * amt
-                break
     
-    return romanNum
+    return result
 
 
 #Start of testing area
 
 # This checks all the Roman Numerals in the range 1-3999 to see if my converters work.
+tot = True
 for i in range(1, 4000):
     r = intToRoman(i)
     n = romanToInt2(r)
     if n!= i:
         print("{}: {}: {}.".format(i, r, n))
-print("Successful 1-3999 loop if not print statements above.")
+        tot = False
+if tot:
+    print("Roman Numeral converters were successful for numbers 1-3999.")
+else:
+    print("Roman Numeral converters failed for numbers 1-3999.  See reasons above.")
+
 
 
 """
@@ -215,7 +186,7 @@ while number != "q":
 
 # The longest roman numeral is 15 digits. So D can be [2..16] looping through
 # just 1 digit or all the way up to 15 digits.
-D = 9
+D = 8
 N = 7
 for i in range(1, D):
     # There are seven possible roman numerals (I, V, X, L, C, D, M).
